713df520d3eff4ccd2dabc7d70e3a67f203e4ca4
maxiamikel19 <maxloversist19@gmail.com>
2024-07-26 10:07:50 -0300

    Estruturar o app

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index(){
+        // dd("show");
+        // exit;
+
+        $users = User::get();
+
+        return view('users.index', ['users' => $users]);
+    }
 }
